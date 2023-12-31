f72896a7a73110ba281cd2d8f52aead8ecc54fb7
zzzzz <150754545+Ancheless@users.noreply.github.com>
2023-12-31 11:59:15 -0500

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -16,12 +16,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply('_*< ANTI-TOXIC />*_\n\n*[ ℹ️ ] ' + `${user.warn == 1 ? `@${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, enviar la palabra "${isToxic}" está prohibido en este grupo.\n\n▢ *Advertencia:* ${user.warn}/5` + '*', false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply('_*< ANTI-TOXIC />*_\n\n*[ ❗ ] ' + `${user.warn == 1 ? `@${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, 𝐞𝐧𝐯𝐢𝐚𝐫 𝐥𝐚 𝐩𝐚𝐥𝐚𝐛𝐫𝐚 "${isToxic}" 𝐞𝐬𝐭á 𝐩𝐫𝐨𝐡𝐢𝐛𝐢𝐝𝐨 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n\n▢ *𝐀𝐝𝐯𝐞𝐫𝐭𝐞𝐧𝐜𝐢𝐚:* ${user.warn}/5` + '*', false, {mentions: [m.sender]});
   }
 
   if (user.warn >= 5) {
     user.warn = 0;
-    await m.reply(`_*< ANTI-TOXIC />*_\n\n*[ ℹ️ ] El participante @${m.sender.split('@')[0]}, superó las 5 advertencias, por lo que será expulsado del grupo.*`, false, {mentions: [m.sender]});
+    await m.reply(`_*< ANTI-TOXIC />*_\n\n*[ ❗ ] 𝐄𝐥 𝐩𝐚𝐫𝐭𝐢𝐜𝐢𝐩𝐚𝐧𝐭𝐞 @${m.sender.split('@')[0]}, 𝐬𝐮𝐩𝐞𝐫ó 𝐥𝐚𝐬 𝟓 𝐚𝐝𝐯𝐞𝐫𝐭𝐞𝐧𝐜𝐢𝐚𝐬, 𝐩𝐨𝐫 𝐥𝐨 𝐪𝐮𝐞 𝐬𝐞𝐫á 𝐞𝐱𝐩𝐮𝐥𝐬𝐚𝐝𝐨 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐨.*`, false, {mentions: [m.sender]});
     user.banned = true;
     await mconn.conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')

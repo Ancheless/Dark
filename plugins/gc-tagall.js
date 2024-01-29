dcab457a5e5d390a218fdef9963df83e1dab9e3c
zzzzz <150754545+Ancheless@users.noreply.github.com>
2024-01-29 16:23:38 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,7 +8,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
   let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
-    teks += `┣➥🍥 @${mem.id.split('@')[0]}\n`;
+    teks += `┣➥🧡 @${mem.id.split('@')[0]}\n`;
   }
   teks += `*└* 𝐁𝐲 @𝐜𝐚𝐥𝐥𝐦𝐞.𝐚𝐧𝐠𝐞𝐥𝐳\n\n**`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
